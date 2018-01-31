f060b28d5634457bd7e1fb0249f22cd67123a6f5
Andrew Shopland <hi@ashp.land>
2018-01-30 18:43:12 -0800

    Initial Commit

---
+++ xwapcore/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xwapcore
+//
+//  Created by Andrew on 2018-01-30.
+//  Copyright © 2018 hearthedge. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
